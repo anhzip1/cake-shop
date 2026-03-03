172e3b4b473e30f7b8e6f2b9d8d7421fb83a55f3
anhzip1 <quocanhtruong191@gmail.com>
2026-03-03 23:40:46 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container mt-5 text-center">
+        <h1 className="text-danger">Cake Shop 🎂</h1>
+        <button className="btn btn-primary mt-3">Đặt bánh ngay</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
